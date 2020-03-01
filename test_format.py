ec36dfcba977ae2c9e4b6bbb0190f804a69c68ad
samjkwong <samkwong@stanford.edu>
2020-03-01 12:39:47 -0800

    Add ucf test split

---
+++ test_format.py
@@ -0,0 +1,22 @@
+"""
+    * Adds class idx label to each line representing a video sample, to match train/val split file format
+    * Writes to test_split.txt
+"""
+
+import os
+
+root = '/vision/group/video/scratch/ucf101_old'
+with open(os.path.join(root, 'classInd.txt'), 'r') as f:
+    class_to_idx = {} # maps class name to class idx
+    for line in f:
+        line = line.split()
+        class_to_idx[line[1]] = line[0]
+        
+with open(os.path.join(root, 'testlist01.txt'), 'r') as f:
+    lines = f.readlines()
+
+if os.path.exists('test_split.txt'):
+    os.remove('test_split.txt')
+with open('test_split.txt', 'w') as f:
+    for line in lines:
+        f.write(line.strip('\n') + ' ' + class_to_idx[line.split('/')[0]] + '\n')
